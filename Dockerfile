843b5c5367d520b7dc6fdbb58d33881eae09f834
Orion <contact@oriondev.fr>
2025-04-21 18:51:02 +0200

    Dockerfile + PAGE LIMIT

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM oven/bun
+
+WORKDIR /app
+
+COPY package.json .
+COPY bun.lock .
+
+RUN bun install --production
+
+COPY src src
+COPY tsconfig.json .
+# COPY public public
+
+ENV NODE_ENV production
+ENV PAGE_LIMIT 5
+CMD ["bun", "src/index.ts"]
+
+EXPOSE 3000
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,32 +1,39 @@
-import * as cheerio from "cheerio"; 
+import * as cheerio from "cheerio";
 
-export async function fetchItems(url: string, items: number, classs: string, firstItems?: any[]) {
-    let itemList: any[] = [];
-    if (firstItems) {
-        itemList = [...firstItems];
-    }
-    let $perPage = {};
-    let page = 1;
+const PAGE_LIMIT = parseInt(process.env.PAGE_LIMIT || "5");
 
-    while (itemList.length < items) {
-        const req= await fetch(`${url}?page=${page}`);
-        if (!req.ok && page === 1) throw new Error(`Failed to fetch news feed`);
-        if (!req.ok && page > 1) continue;
+export async function fetchItems(
+  url: string,
+  items: number,
+  classs: string,
+  firstItems?: any[],
+) {
+  let itemList: any[] = [];
+  if (firstItems) {
+    itemList = [...firstItems];
+  }
+  let $perPage = {};
+  let page = 1;
 
-        let html = await req.text();
-        let $ = cheerio.load(html);
-        let pageItemsRaw = $(classs);
-        let pageItems = pageItemsRaw.map((index, item) => ({ page, item }));
-        // @ts-ignore
-        $perPage[page] = $;
-  
-        itemList.push(...pageItems);
-  
-        page++;
-    }
+  while (itemList.length < items && page <= PAGE_LIMIT) {
+    const req = await fetch(`${url}?page=${page}`);
+    if (!req.ok && page === 1) throw new Error(`Failed to fetch news feed`);
+    if (!req.ok && page > 1) continue;
 
-    return {
-        items: itemList,
-        $perPage
-    }
+    let html = await req.text();
+    let $ = cheerio.load(html);
+    let pageItemsRaw = $(classs);
+    let pageItems = pageItemsRaw.map((index, item) => ({ page, item }));
+    // @ts-ignore
+    $perPage[page] = $;
+
+    itemList.push(...pageItems);
+
+    page++;
+  }
+
+  return {
+    items: itemList,
+    $perPage,
+  };
 }
